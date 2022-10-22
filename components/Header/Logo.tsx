4604b376953fb768d5ce728f76942801ffa9acc0
Sim <simmorsal@gmail.com>
2022-10-22 05:32:07 +0330

    fixed header logo recoloring issue by rewriting it with Emotion

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -1,55 +1,104 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { css } from "@emotion/react";
 import { motion } from "framer-motion";
 
 export default function Logo() {
-  // ! for some reason color transition has no animation. Investigate why
-  const variants = (secondaryColor: string, delay: number) => {
-    return {
-      initial: {
-        color: "black",
-      },
-      animate: {
-        color: secondaryColor,
-        transition: {
-          duration: 0.7,
-          delay: 1 + 0.2 * delay,
-        },
-      },
-    };
-  };
-
   const colors = ["#b21451", "#188b9b", "#e38c15"];
+  const [beginAnimation, setBeginAnimation] = useState(false);
+
+  useEffect(() => {
+    // Sometimes when immediately animating from beginning of page
+    // load, the animation will jump to the end and the transition
+    // won't be showing, specially on phones. However waiting a
+    // little will get rid of this problem.
+    setTimeout(() => {
+      setBeginAnimation(true);
+    }, 50);
+  }, []);
 
   return (
-    <motion.p
+    <p
       css={css`
         font-weight: bold;
         font-family: monospace;
         font-size: 48px;
         height: fit-content;
+        user-select: none;
+        opacity: ${beginAnimation ? 1 : 0};
+        transform: translateX(${beginAnimation ? 0 : -110}px);
+
+        transition: 800ms ease;
+        transition-property: transform, opacity;
       `}
-      initial={{
-        translateX: -110,
-        opacity: 0,
-      }}
-      animate={{
-        translateX: 0,
-        opacity: 1,
-      }}
     >
       {["A", "B", "C"].map((value, index) => {
         return (
-          <motion.span
+          <span
             key={value}
-            variants={variants(colors[index], index)}
-            initial={"initial"}
-            animate={"animate"}
+            css={css`
+              color: ${beginAnimation
+                ? colors[index]
+                : "#222222"};
+
+              transition: color 700ms ease;
+              transition-delay: ${1000 + index * 300}ms;
+            `}
           >
             {value}
-          </motion.span>
+          </span>
         );
       })}
-    </motion.p>
+    </p>
   );
+
+  // ! for some reason color transition has no animation. Investigate why
+
+  // const variants = (secondaryColor: string, delay: number) => {
+  //   return {
+  //     initial: {
+  //       color: "black",
+  //     },
+  //     animate: {
+  //       color: secondaryColor,
+  //       transition: {
+  //         duration: 0.7,
+  //         delay: 1 + 0.2 * delay,
+  //       },
+  //     },
+  //   };
+  // };
+  //
+  // const colors = ["#b21451", "#188b9b", "#e38c15"];
+  //
+  // return (
+  //   <motion.p
+  //     css={css`
+  //       font-weight: bold;
+  //       font-family: monospace;
+  //       font-size: 48px;
+  //       height: fit-content;
+  //     `}
+  //     initial={{
+  //       translateX: -110,
+  //       opacity: 0,
+  //     }}
+  //     animate={{
+  //       translateX: 0,
+  //       opacity: 1,
+  //     }}
+  //   >
+  //     {["A", "B", "C"].map((value, index) => {
+  //       return (
+  //         <motion.span
+  //           key={value}
+  //           variants={variants(colors[index], index)}
+  //           initial={"initial"}
+  //           animate={"animate"}
+  //         >
+  //           {value}
+  //         </motion.span>
+  //       );
+  //     })}
+  //   </motion.p>
+  // );
 }
